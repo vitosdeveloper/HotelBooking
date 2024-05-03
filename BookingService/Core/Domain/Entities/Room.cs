15e77837ce711ee0ec440799961a78b084ba3c12
vitosnatios <vitosdeveloper@gmail.com>
2024-05-03 00:22:38 -0300

    Created the domain entities and a State Machine to handle the Booking.Status

---
+++ BookingService/Core/Domain/Entities/Room.cs
@@ -0,0 +1,28 @@
+﻿namespace Domain.Entities
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public bool InMaintenance { get; set; }
+
+        public bool IsAvailable
+        {
+            get
+            {
+                if (InMaintenance || !HasGuest)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        public bool HasGuest
+        {
+            // verificar se existem bookings abertos pra essa room
+            get { return true; }
+        }
+    }
+}
